e9eff032b69e38fc7b100c7467f18487331fbc63
Mustafa Bahadır Doğrusöz <62994896+BahadirDogrusoz@users.noreply.github.com>
2021-08-27 14:57:09 +0300

    Decorators

---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+
+
+# Python Decorator Function
+
+def delay_decorator(function):
+    def wrapper_function():
+        time.sleep(2)
+        function()
+        function()
+
+    return wrapper_function
+
+
+@delay_decorator
+def say_hello():
+    print("Hello")
+
+
+@delay_decorator
+def say_bye():
+    print("Bye")
+
+
+def say_greeting():
+    print("How are you?")
+
+
+say_hello()
+
+decorated_function = delay_decorator(say_greeting)
+decorated_function()
